a10d44f21182b985b83f5324cb5d52ac93ca785d
Szabolcs Beki <szabolcs.beki@gmail.com>
2025-12-27 00:15:33 +0100

    feat: add deploy script for lp-backtester

---
+++ lp-backtester/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export CHAINTLIGENCE_HOME="/home/szabi/git/chaintelligence/"
+export LP_BACKTESTER_HOME="${CHAINTLIGENCE_HOME}/lp-backtester/"
+export LP_BACKTESTER_WEB_HOME="/var/www/lp-backtester/html/"
+cd ${CHAINTLIGENCE_HOME}
+git pull
+cp -r ${LP_BACKTESTER_HOME}/*  ${LP_BACKTESTER_WEB_HOME}
