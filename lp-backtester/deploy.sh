e83bd1b76eddd28bf69235b92532414731d2c2b4
Szabolcs Beki <szabolcs.beki@gmail.com>
2025-12-27 17:09:17 +0100

    Delete www folder at deply

--- lp-backtester/deploy.sh
+++ lp-backtester/deploy.sh
@@ -5,4 +5,5 @@ export LP_BACKTESTER_HOME="${CHAINTLIGENCE_HOME}/lp-backtester/"
 export LP_BACKTESTER_WEB_HOME="/var/www/lp-backtester/html/"
 cd ${CHAINTLIGENCE_HOME}
 git pull
+rm -rf ${LP_BACKTESTER_WEB_HOME}/*
 cp -r ${LP_BACKTESTER_HOME}/*  ${LP_BACKTESTER_WEB_HOME}
